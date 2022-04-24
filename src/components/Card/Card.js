1d6ec88a32f82df342ecc74f542200778e68ec2d
imroz-19 <imroz.manzar19@gmail.com>
2022-04-24 06:34:34 +0530

    Added initial code setup, Added home page, generic reusable card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div className='card-container'>
+      <div className='card-content'>
+        <h1></h1>
+        <span></span>
+      </div>
+    </div>
+  )
+}
